c1f62e98b0d68865954cf7eae36acddb90a5a1e1
LeuBorelli <lej.borelli@gmail.com>
2026-02-21 18:24:46 -0300

    Initial commit - API, WEB and Playwright tests

---
+++ apps/zombieplus/api/src/config/database.js
@@ -0,0 +1,22 @@
+require('../bootstrap');
+
+module.exports = {
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
